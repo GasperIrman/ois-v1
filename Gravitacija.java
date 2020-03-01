113ca5ed9ae52cb783d90c679a82acfa1ea1820e
GasperIrman <gasper.irman@gmail.com>
2020-03-01 16:21:00 +0000

    Dodajanje izračuna pospeška

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,13 @@
+import java.lang.Math;
+
+
 public class Gravitacija {
     public static void main(String[] args) {
-        System.out.println("OIS je zakon!");
+
+    }
+    
+    public static double izracunajPospesek(int nadVisina) {
+        double a = ((double) 6.674 * 5.972 * Math.pow(10, -11) * Math.pow(10, 24)) / Math.pow(6.371 * Math.pow(10, 6) + nadVisina, 2);
+        return a;
     }
 }
